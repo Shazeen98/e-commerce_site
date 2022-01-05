518f356ff9174d36ec282585784184f6b10deaf8
Shazeen98 <mhmmdshazeen@gmail.com>
2022-01-05 15:50:33 +0530

    feat:customer_count

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,9 +1,28 @@
 import React,{Component} from "react";
 
 export default class MainContent extends Component{
+
+    state ={pageTitle: "Customers",customersCount:5};
+
+
     render() {
-        return<div>Hello from Main content</div>
+        return(
+            <div>
+                <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}
+                <span className="badge badge-secondary m-2">{this.state.customersCount}</span>
+                    <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
+                </h4>
+            </div>
+        )
     }
+
+    onRefreshClick=()=>{
+            this.setState({
+                customersCount:7,
+
+            });
+        }
+
 
 
 }
