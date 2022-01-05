62cd6a42ef4cb1219c4dddb50e27eb66aaeababa
shazeen <shazeen@orelit.com>
2022-01-05 13:20:00 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from"react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
+import"./index.css";
+
+
+ReactDom.render(<NavBar/>,document.getElementById
+("root"));
